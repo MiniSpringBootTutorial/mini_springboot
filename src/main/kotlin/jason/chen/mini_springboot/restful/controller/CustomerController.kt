01e28a9affb0fffae35d87d3afbfd0cf2d87c8b5
jack <guangjian.cgj@alibaba-inc.com>
2017-03-11 21:48:35 +0800

    front 2 end , use thymeleaf

--- src/main/kotlin/jason/chen/mini_springboot/restful/controller/CustomerController.kt
+++ src/main/kotlin/jason/chen/mini_springboot/restful/controller/CustomerController.kt
@@ -1,17 +1,33 @@
 package jason.chen.mini_springboot.restful.controller
 
-import jason.chen.mini_springboot.restful.biz.CustomerRepository
+import jason.chen.mini_springboot.restful.biz.CustomerService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController
-class CustomerController(val repository: CustomerRepository) {
+@Controller
+class CustomerController(val customerService: CustomerService) {
 
-	@GetMapping("/")
-	fun findAll() = repository.findAll()
+    @GetMapping(value = "/")
+    fun index(): String {
+        return "index"
+    }
 
-	@GetMapping("/{lastName}")
-	fun findByLastName(@PathVariable lastName:String)
-			= repository.findByLastName(lastName)
+    @GetMapping("/customers.do")
+    fun listAll(model: Model): String {
+        val allCustomers = customerService.findAll()
+        model.addAttribute("customers", allCustomers)
+        return "customers"
+    }
+
+    @GetMapping("/listCustomers")
+    @ResponseBody
+    fun listCustomers(model: Model) = customerService.findAll()
+
+    @GetMapping("/{lastName}")
+    @ResponseBody
+    fun findByLastName(@PathVariable lastName: String)
+            = customerService.findByLastName(lastName)
 }
