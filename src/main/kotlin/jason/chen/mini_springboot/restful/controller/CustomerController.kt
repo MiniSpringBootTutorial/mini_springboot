9a12ad4130b6465f2bdb37eeba23db229b40f147
jack <guangjian.cgj@alibaba-inc.com>
2017-03-11 22:55:03 +0800

    thymeleaf配置注释说明

--- src/main/kotlin/jason/chen/mini_springboot/restful/controller/CustomerController.kt
+++ src/main/kotlin/jason/chen/mini_springboot/restful/controller/CustomerController.kt
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.ResponseBody
 
+/**
+ * * 注意:
+ *      这里要使用@Controller注解
+ *      而不要使用@RestController
+ *      否则return "index";只是返回字符串"index"， 不能跳转到index.html
+ *
+ *      @RestController is a stereotype annotation that combines @ResponseBody and @Controller.
+ *      @RestController注解相当于@ResponseBody ＋ @Controller合在一起的作用。
+ */
+
 @Controller
 class CustomerController(val customerService: CustomerService) {
 
